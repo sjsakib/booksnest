ef742cf072ea9984906e3e8d359118d45558a3e8
sjsakib <sjsakib.bd@gmail.com>
2020-05-26 21:05:13 +0600

    don't use generic controller'

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { BaseCrudController } from 'src/common/BaseCrud.controller';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { AuthorsService } from './authors.service';
 import { Author } from './author.entity';
+import { IdValidationPipe } from 'src/common/id-validation.pipe';
 
 @Controller('authors')
 export class AuthorsController extends BaseCrudController<
@@ -12,7 +21,35 @@ export class AuthorsController extends BaseCrudController<
   UpdateAuthorDto,
   AuthorsService
 > {
-  constructor(authorsService: AuthorsService) {
+  constructor(private authorsService: AuthorsService) {
     super(authorsService);
+  }
+
+  @Get()
+  books() {
+    return this.authorsService.allItems();
+  }
+
+  @Post()
+  createBook(@Body() createItemDto: CreateAuthorDto) {
+    return this.authorsService.createItem(createItemDto);
+  }
+
+  @Get('/:id')
+  getById(@Param('id', IdValidationPipe) id: string) {
+    return this.authorsService.getById(id);
+  }
+
+  @Patch('/:id')
+  updateItem(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() updateItemDto: UpdateAuthorDto,
+  ) {
+    return this.authorsService.updateItem(id, updateItemDto);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id', IdValidationPipe) id: string) {
+    return this.authorsService.deleteItem(id);
   }
 }
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { BaseCrudController } from 'src/common/BaseCrud.controller';
 import { Book } from './book.entity';
+import { IdValidationPipe } from 'src/common/id-validation.pipe';
 
 @Controller('books')
 export class BooksController extends BaseCrudController<
@@ -12,7 +21,35 @@ export class BooksController extends BaseCrudController<
   UpdateBookDto,
   BooksService
 > {
-  constructor(booksService: BooksService) {
+  constructor(private booksService: BooksService) {
     super(booksService);
+  }
+
+  @Get()
+  books() {
+    return this.booksService.allItems();
+  }
+
+  @Post()
+  createBook(@Body() createItemDto: CreateBookDto) {
+    return this.booksService.createItem(createItemDto);
+  }
+
+  @Get('/:id')
+  getById(@Param('id', IdValidationPipe) id: string) {
+    return this.booksService.getById(id);
+  }
+
+  @Patch('/:id')
+  updateItem(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() updateItemDto: UpdateBookDto,
+  ) {
+    return this.booksService.updateItem(id, updateItemDto);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id', IdValidationPipe) id: string) {
+    return this.booksService.deleteItem(id);
   }
 }
